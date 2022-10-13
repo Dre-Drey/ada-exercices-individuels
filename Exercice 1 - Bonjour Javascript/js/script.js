45639cc337e3cfc686270e7e868aee8307403040
Dre-Drey <51761267+Dre-Drey@users.noreply.github.com>
2022-10-13 16:48:58 +0200

    changement nom var let

--- Exercice 1 - Bonjour Javascript/js/script.js
+++ Exercice 1 - Bonjour Javascript/js/script.js
@@ -1,12 +1,12 @@
 function askName() {
   PromptedName = prompt("Quel est ton nom?", [""]);
-  var nom = "Bonjour " + PromptedName;
+  let nom = "Bonjour " + PromptedName;
   document.getElementById("nom").innerHTML += nom;
 }
 
 function askBirthYear() {
-  var PromptedAge = prompt("Quel est ton année de naissance?", [""]);
-  var age = 2022 - PromptedAge;
+  let PromptedAge = prompt("Quel est ton année de naissance?", [""]);
+  let age = 2022 - PromptedAge;
   document.getElementById("age").innerHTML += "Vous avez " + age + " ans";
 }
 
