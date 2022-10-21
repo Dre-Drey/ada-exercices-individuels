3fb61f927f5d6da19efab6b6f028a13681ae9e5b
Dre-Drey <51761267+Dre-Drey@users.noreply.github.com>
2022-10-21 16:11:03 +0200

    etape 3 et 4 exercice 2

--- Exercice 2 - Guess a number/script.js
+++ Exercice 2 - Guess a number/script.js
@@ -1,14 +1,24 @@
-let givenNumber = parseInt(prompt("Ecris un nombre"));
-let chosenNumber = 22;
+let numberToGuess;
+
+function chosenNumber() {
+  numberToGuess = parseInt(
+    prompt("Joueur 1 : Choisis un nombre entre 0 et 50")
+  );
+  while (numberToGuess > 50 || numberToGuess < 0) {
+    chosenNumber();
+  }
+}
+chosenNumber();
 
 function didIWin() {
-  if (givenNumber < chosenNumber) {
+  let givenNumber = parseInt(prompt("Joueur 2 : Devine le nombre"));
+  if (givenNumber < numberToGuess) {
     alert("Plus grand");
     return false;
-  } else if (givenNumber > chosenNumber) {
+  } else if (givenNumber > numberToGuess) {
     alert("Plus petit");
     return false;
-  } else if (givenNumber === chosenNumber) {
+  } else if (givenNumber === numberToGuess) {
     alert("Bravo ! Vous avez deviné le nombre !");
     return true;
   } else {
@@ -18,11 +28,11 @@ function didIWin() {
 }
 
 function gamePlay() {
-  didIWin();
-  if ((didIWin = true)) {
+  const result = didIWin();
+  if (result == true) {
     alert("Le jeu est fini");
   } else {
-    parseInt(prompt("Ecris un nombre"));
+    gamePlay();
   }
 }
 gamePlay();
