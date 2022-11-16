b3c4c0611e1027d5992b508324e79407d2711418
Dre-Drey <51761267+Dre-Drey@users.noreply.github.com>
2022-11-16 12:16:52 +0100

    exercice 3 - test des fonctions
    html et css de l'exercice 3 avec button test des fonctions

--- Exercice 3 - morse/script.js
+++ Exercice 3 - morse/script.js
@@ -25,15 +25,93 @@ const latinToMorse = {
   X: "-..-",
   Y: "-.--",
   Z: "--..",
+  " ": " ",
 };
 
-function getLatinCharacter(sentenceToSplit, separator) {
-  var arrayOfCharacters = sentenceToSplit.split(separator);
-  console.log(arrayOfCharacters);
-  document.getElementById("morse").innerHTML = arrayOfCharacters;
+const morseToLatin = {
+  "-": "T",
+  "--": "M",
+  "---": "O",
+  "--.": "G",
+  "--.-": "Q",
+  "--..": "Z",
+  "-.": "N",
+  "-.-": "K",
+  "-.--": "Y",
+  "-.-.": "C",
+  "-..": "D",
+  "-..-": "X",
+  "-...": "B",
+  ".": "E",
+  ".-": "A",
+  ".--": "W",
+  ".---": "J",
+  ".--.": "P",
+  ".-.": "R",
+  ".-..": "L",
+  "..": "I",
+  "..-": "U",
+  "..-.": "F",
+  "...": "S",
+  "...-": "V",
+  "....": "H",
+};
+
+//Etape 1 : spliter une phrase caractères majuscules stockés dans un tableau.
+function getLatinCharacterList(sentenceToSplit, separator) {
+  let sentenceToSplitMaj = sentenceToSplit.toUpperCase();
+  return sentenceToSplitMaj.split(separator);
 }
 
-let sentence = "Je m'appelle Audrey";
-let character = "";
+console.log(getLatinCharacterList("Hello World", ""));
+document.getElementById("split").innerHTML = getLatinCharacterList(
+  "Hello, World",
+  ""
+);
 
-getLatinCharacter(sentence, character);
+/* Autre possibilité : 
+function getLatinCharacterList(str) { 
+  return Array.from(str);
+} */
+
+//Etape 2 : aller prendre la valeur en morse d'un caractere latin
+function translateLatinCharacter(latinCharacter) {
+  return (morseCharacter = latinToMorse[latinCharacter]);
+}
+document.getElementById("morse").innerHTML = translateLatinCharacter("H");
+
+//Etape 3 : transformer une phrase en caract. latins en morse.
+function encode(latinTexte) {
+  let morsePhrase = "";
+  for (i = 0; i < getLatinCharacterList(latinTexte, "").length; i++) {
+    morsePhrase +=
+      " " + translateLatinCharacter(getLatinCharacterList(latinTexte, "")[i]) "/";
+  }
+  return morsePhrase;
+}
+
+encode("Hello World");
+document.getElementById("phrase").innerHTML = encode("Hello World");
+
+//Etape 4 : créer une fonction decode qui transforme du morse en caract. latins.
+
+function getMorseCharacterList(sentenceToSplit, separator) {
+  return sentenceToSplit.split(separator);
+}
+getMorseCharacterList("-- .-", " ");
+console.log(getMorseCharacterList("-- .-", " "));
+
+function translateMorseCharacter(morse) {
+  return (latinCharacter = morseToLatin[morse]);
+}
+console.log(translateMorseCharacter("--"));
+
+function decode(morseTexte) {
+  let morsePhrase = "";
+  for (i = 0; i < getMorseCharacterList(morseTexte, " ").length; i++) {
+    morsePhrase +=
+      " " + translateMorseCharacter(getMorseCharacterList(morseTexte, " ")[i]);
+  }
+  return morsePhrase;
+}
+console.log(decode("-- .-"));
